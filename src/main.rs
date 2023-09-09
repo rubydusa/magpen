62bbcbbde5cab328206c1b42e3e3072e5b241b9e
rubydusa <rubydusa@protonmail.com>
2023-09-09 17:20:16 +0300

    smoother trail

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ impl PhysicsContext {
         PhysicsContext { 
             gravity: 10., 
             pixels_per_meter: 3000., 
-            magnet_coefficent: 0.0004,
+            magnet_coefficent: 0.0002,
             time_precision: 0.001 
         }
     }
@@ -44,6 +44,7 @@ struct Ball {
     velocity: Vec3,
     air_friction: f32,
     magnets: Vec<Vec3>,
+    last_positions: Vec<Vec2>,
 }
 
 impl Ball {
@@ -54,39 +55,53 @@ impl Ball {
         b
     }
 
-    fn move_self(&mut self, time_delta: f32, physics_ctx: &PhysicsContext) {
+    fn move_step(&mut self, physics_ctx: &PhysicsContext) {
+        let ball_pos = vec3(self.pos.x, self.pos.y, self.ball_height());
+        let gravity_force = vec3(0., 0., -1. * physics_ctx.gravity * self.mass);
+        let friction_force = self.velocity.normalize_or_zero() * self.velocity.length_squared() * self.air_friction * -1.;
+
+        let mut magnetic_force = vec3(0., 0., 0.);
+        for magnet in self.magnets.iter() {
+            let magnet_force = *magnet - ball_pos; 
+            let magnitude = physics_ctx.magnet_coefficent / magnet_force.length_squared();
+            let magnet_force = magnet_force.normalize() * magnitude;
+
+            magnetic_force += magnet_force;
+        }
+
+        let force_vector = gravity_force + magnetic_force + friction_force;
+        let rope_vector = self.rope_pivot - ball_pos;
+
+        let force_projected = (force_vector.dot(rope_vector) / rope_vector.length_squared()) * rope_vector;
+        let angle = angle3(force_projected, force_vector).to_degrees();
+        let force_projected = if angle < 90. {
+            force_projected * -1.
+        } else {
+            force_projected
+        };
+
+        let force = force_vector + force_projected;
+
+        let a = force / self.mass;
+        self.velocity += a * physics_ctx.time_precision;
+        self.pos += self.velocity.xy() * physics_ctx.time_precision;
+    }
+
+    fn move_over_time(&mut self, time_delta: f32, physics_ctx: &PhysicsContext) {
         let times = (time_delta / physics_ctx.time_precision).floor() as u32;
         for _ in 0..times {
-            let ball_pos = vec3(self.pos.x, self.pos.y, self.ball_height());
-            let gravity_force = vec3(0., 0., -1. * physics_ctx.gravity * self.mass);
-            let friction_force = self.velocity.normalize_or_zero() * self.velocity.length_squared() * self.air_friction * -1.;
-
-            let mut magnetic_force = vec3(0., 0., 0.);
-            for magnet in self.magnets.iter() {
-               let magnet_force = *magnet - ball_pos; 
-               let magnitude = physics_ctx.magnet_coefficent / magnet_force.length_squared();
-               let magnet_force = magnet_force.normalize() * magnitude;
-
-               magnetic_force += magnet_force;
-            }
-
-            let force_vector = gravity_force + magnetic_force + friction_force;
-            let rope_vector = self.rope_pivot - ball_pos;
-
-            let force_projected = (force_vector.dot(rope_vector) / rope_vector.length_squared()) * rope_vector;
-            let angle = angle3(force_projected, force_vector).to_degrees();
-            let force_projected = if angle < 90. {
-                force_projected * -1.
-            } else {
-                force_projected
-            };
-
-            let force = force_vector + force_projected;
-
-            let a = force / self.mass;
-            self.velocity += a * physics_ctx.time_precision;
-            self.pos += self.velocity.xy() * physics_ctx.time_precision;
+            self.move_step(physics_ctx);
         }
+    }
+
+    fn move_over_time_save_positions(&mut self, time_delta: f32, physics_ctx: &PhysicsContext) {
+        let times = (time_delta / physics_ctx.time_precision).floor() as u32;
+        let positions: Vec<_> = (0..times).map(|_| {
+            self.move_step(physics_ctx);
+            self.pos.clone()
+        }).collect();
+
+        self.last_positions = positions;
     }
 }
 
@@ -152,7 +167,8 @@ impl State {
                     vec3((30.0 as f32).to_radians().cos(), (30.0 as f32).to_radians().sin(), 1.) * 0.04,
                     vec3((150.0 as f32).to_radians().cos(), (150.0 as f32).to_radians().sin(), 1.) * 0.04,
                     vec3((270.0 as f32).to_radians().cos(), (270.0 as f32).to_radians().sin(), 1.) * 0.04
-                ]
+                ],
+                last_positions: vec![]
             },
             meshes: Meshes::new(ctx),
             physics_ctx: PhysicsContext::new()
@@ -160,7 +176,7 @@ impl State {
     }
 
     fn update(&mut self, ctx: &mut Context) {
-        self.ball.move_self(ctx.time.delta().as_secs_f32(), &self.physics_ctx);
+        self.ball.move_over_time_save_positions(ctx.time.delta().as_secs_f32(), &self.physics_ctx);
     }
 }
 
@@ -174,10 +190,14 @@ impl ggez::event::EventHandler<GameError> for State {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> { 
-        let pos = canvas_position(self.ball.pos, ctx, &self.physics_ctx);
-
         let mut trail_canvas = Canvas::from_screen_image(ctx, &mut self.trail, None);
-        trail_canvas.draw(&self.meshes.trail, pos);
+
+        let mut last_pos = vec2(0., 0.);
+        for pos in self.ball.last_positions.drain(0..).map(|x| canvas_position(x, ctx, &self.physics_ctx)) {
+            trail_canvas.draw(&self.meshes.trail, pos);
+            last_pos = pos;
+        }
+
         trail_canvas.finish(&mut ctx.gfx)?;
 
         let mut canvas = Canvas::from_frame(ctx, Color::WHITE);
@@ -185,7 +205,7 @@ impl ggez::event::EventHandler<GameError> for State {
         for magnet in self.ball.magnets.iter() {
             canvas.draw(&self.meshes.magnet, canvas_position(magnet.xy(), ctx, &self.physics_ctx))
         }
-        canvas.draw(&self.meshes.ball, pos);
+        canvas.draw(&self.meshes.ball, last_pos);
 
         canvas.finish(&mut ctx.gfx)?;
 
